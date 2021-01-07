06a11aba8ef83e15e27e2694eee230029240ee1e
Vanessa <vanessa.nunez.p@gmail.com>
2021-01-06 21:55:13 -0300

    create new models

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Documento
 from .models import Person
+from .models import Produto
+from .models import Venda
 
+admin.site.register(Documento)
 admin.site.register(Person)
+admin.site.register(Produto)
+admin.site.register(Venda)
--- clientes/models.py
+++ clientes/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Documento(models.Model):
+    num_doc = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.num_doc
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -7,6 +14,27 @@ class Person(models.Model):
     salary = models.DecimalField(max_digits=6, decimal_places=2)
     bio = models.TextField()
     photo = models.ImageField(upload_to='clients_photos', null=True, blank=True)
+    doc = models.OneToOneField(Documento, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
+        return f'{self.first_name} {self.last_name}'
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
+
+
+class Venda(models.Model):
+    numero = models.CharField(max_length=7)
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
+    desconto = models.DecimalField(max_digits=5, decimal_places=2)
+    imposto = models.DecimalField(max_digits=5, decimal_places=2)
+    pessoa = models.ForeignKey(Person, null=True, blank=True, on_delete=models.PROTECT)
+    produtos = models.ManyToManyField(Produto, null=True, blank=True)
+
+    def __str__(self):
+        return f'#{self.numero} - {self.valor} ({self.pessoa})'
